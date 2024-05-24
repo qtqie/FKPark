2765fbd8a110c0c9c825e89dee6542a3045a0b57
IzzahNia <122985082+IzzahNia@users.noreply.github.com>
2024-05-25 00:56:39 +0800

    Update AddBooking.php

--- Manage Parking Booking/AddBooking.php
+++ Manage Parking Booking/AddBooking.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Booking Page</title>
+<title>Add Booking Page</title>
 <style>
 * {
   box-sizing: border-box;
