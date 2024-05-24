dc9abce9c1725f21951fc7d2e092886c27a124e3
qtqie <165252505+qtqie@users.noreply.github.com>
2024-05-24 21:36:26 +0800

    Update UserRegistration.php

--- Manage User Login/UserRegistration.php
+++ Manage User Login/UserRegistration.php
@@ -1,1 +1,115 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="viewexpert.css">
+    <title>FKPark</title>
+    <link href="https://png.pngtree.com/png-clipart/20190619/original/pngtree-vector-car-icon-png-image_3989896.jpg" rel="icon">
+    <style>
+      html, body {
+        height: 100%;
+        background-color: #800000;
+      }
+    </style>
+  </head>
+  <body>
+    <section class="vh-100" style="background-color: #800000;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col col-xl-10">
+            <div class="card h-100" style="border-radius: 1rem;">
+              <div class="row g-0 h-100">
+                <div class="col-md-6 col-lg-5 d-none d-md-block">
+                  <img src="https://i.pinimg.com/originals/2c/d8/9e/2cd89ed550ec3424e2eb3c6e036a1cb7.jpg" alt="login form" class="img-fluid h-100" style="border-radius: 1rem 0 0 1rem; object-fit: cover;" />
+                </div>
+                <div class="col-xl-6">
+                  <div class="card-body p-md-5 text-black">
+                    <h3 class="mb-5 text-uppercase">Vehicle registration form</h3>
 
+                    <div data-mdb-input-init class="form-outline mb-4">
+                      <input type="text" id="form3Example8" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example8">Full Name</label>
+                    </div>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                      <input type="text" id="form3Example8" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example8">Password</label>
+                    </div>
+
+
+                    <div class="row">
+                      <div class="col-md-6 mb-4">
+                        <div data-mdb-input-init class="form-outline">
+                          <input type="text" id="form3Example1m1" class="form-control form-control-lg" />
+                          <label class="form-label" for="form3Example1m1">Vehicle's number plate</label>
+                        </div>
+                      </div>
+                      <div class="col-md-6 mb-4">
+                        <div data-mdb-input-init class="form-outline">
+                          <input type="text" id="form3Example1n1" class="form-control form-control-lg" />
+                          <label class="form-label" for="form3Example1n1">Student/Staff ID</label>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6 mb-4">
+                        <select data-mdb-select-init class="form-select">
+                          <option value="1">Vehicle Type</option>
+                          <option value="2">MPV</option>
+                          <option value="3">Car</option>
+                          <option value="4">Motorcycle</option>
+                        </select>
+                      </div>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                      <input type="text" id="form3Example8" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example8">Address</label>
+                    </div>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                      <input type="text" id="form3Example97" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example97">Email ID</label>
+                    </div>
+
+                    <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
+                      <h6 class="mb-0 me-4">Gender: </h6>
+                      <div class="form-check form-check-inline mb-0 me-4">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender" value="option1" />
+                        <label class="form-check-label" for="femaleGender">Female</label>
+                      </div>
+                      <div class="form-check form-check-inline mb-0 me-4">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender" value="option2" />
+                        <label class="form-check-label" for="maleGender">Male</label>
+                      </div>
+                    </div>
+
+                      <div class="col-md-6 mb-4">
+                        <select data-mdb-select-init class="form-select">
+                          <option value="1">User Type</option>
+                          <option value="2">Student</option>
+                          <option value="3">Staff</option>
+                          <option value="4">Administrator</option>
+                        </select>
+                      </div>
+                    </div>
+
+
+                    <div class="d-flex justify-content-end pt-3">
+                      <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-light btn-lg">Reset all</button>
+                      <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-warning btn-lg ms-2">Submit form</button>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
